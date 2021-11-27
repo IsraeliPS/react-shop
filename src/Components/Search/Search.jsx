c0a335201e36e3bdf449e8d9b9ec02458ad8eb14
Miguel.Contreras <contreras.izquierdo.miguel@gmail.com>
2021-11-26 19:20:00 -0600

    adding component Search

---
+++ src/Components/Search/Search.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+const Search =()=>{
+
+    return
+    <div>
+        <input className="" 
+        type="text" 
+        placeholder="Search" 
+        aria-label="Search"
+        // value={buscar}
+        // onChange={(e)=>setBuscar(e.target.value)}
+        >
+
+        </input>
+    </div>
+}
+
+export default Search;
