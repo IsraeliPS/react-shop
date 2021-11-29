e6e4ac23b7b08fca8fe49e50bca91c6ef57cf800
Israelí Pérez <israeli.ps@gamil.com>
2021-11-28 23:08:03 -0600

    modificando shoppingCar

--- src/Components/ShoppingCar/ShoppingCar.jsx
+++ src/Components/ShoppingCar/ShoppingCar.jsx
@@ -1,21 +1,61 @@
-import React from 'react'
-import styles from './ShoppingCar.module.scss'
+// import React, { useState } from 'react'
+// import styles from './ShoppingCar.module.scss'
+// import 'bootstrap/dist/css/bootstrap.min.css'
 
-const ShoppingCar = ({ emptyCart, cartItems }) => {
-  const totalPrice = Object.values(cartItems).reduce((sum, item) => sum + parseInt(item.price), 0)
-  const totalItems = Object.values(cartItems).reduce((sum, item) => sum + parseInt(item.qty), 0)
+// import { Link, useLocation } from 'react-router-dom'
 
-  return (
-    <div className={`${styles.ShoppingCar}`}>
-      <div>
-        <span className={`${styles.materialIcons}`}>shopping_cart</span>
-        <span className={`${styles.cantidad}`}>{totalItems}</span>
-        <div>Total a pagar: {totalPrice}</div>
-      </div>
+// import {
+//   Badge,
+//   Dropdown,
+//   DropdownItem,
+//   DropdownToggle,
+//   DropdownMenu
+// } from 'reactstrap'
 
-      <button className={`${styles.button} ${styles.materialIcons}`} onClick={emptyCart}>delete</button>
-    </div>
-  )
-}
+// const ShoppingCar = ({ emptyCart, cartItems }) => {
+//   const [dropdown, setDropdown] = useState(false)
+//   const totalPrice = Object.values(cartItems).reduce(
+//     (sum, item) => sum + parseInt(item.price),
+//     0
+//   )
+//   const totalItems = Object.values(cartItems).reduce(
+//     (sum, item) => sum + parseInt(item.qty),
+//     0
+//   )
 
-export default ShoppingCar
+//   const openCloseDropdown = () => {
+//     setDropdown(!dropdown)
+//   }
+//   // let cantidad = 0;
+//   // let total = 0;
+//   // for (let val in cartItems) {
+//   //   total += parseInt(cartItems[val].price);
+//   //   cantidad += cartItems[val].qty;
+//   // }
+
+//   return (
+//     <>
+//       <Dropdown isOpen={dropdown} toggle={openCloseDropdown}>
+//         <DropdownToggle caret>
+//           Dropdown
+//         </DropdownToggle>
+//         <DropdownMenu>
+//           <DropdownItem header>
+//             Productos
+//           </DropdownItem>
+//           <DropdownItem divider />
+//           {cartItems.map((item) => {
+//             const { _id, name, img, price } = item
+//             return (
+//               <DropdownItem>
+//                 <Badge>{item.length}</Badge>
+//               </DropdownItem>
+//             )
+//           })}
+//         </DropdownMenu>
+//       </Dropdown>
+//     </>
+//   )
+// }
+
+// export default ShoppingCar
--- src/Components/atole/atoles.jsx
+++ src/Components/atole/atoles.jsx
@@ -1,53 +1,53 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react'
 
-import Card from "./CardAtole/CardAtole";
+import Card from './CardAtole/CardAtole'
 
-const AtolesData = "https://api-cafe-tamales.herokuapp.com/api/atoles";
+const AtolesData = 'https://api-cafe-tamales.herokuapp.com/api/atoles';
 
-const AtoleIndex = ({addToCart, cartItems, removeFromCart}) => {
-  const [atole, setAtole] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
+const AtoleIndex = ({ addToCart, cartItems, removeFromCart }) => {
+  const [atole, setAtole] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
 
   useEffect(() => {
-    let ComponentExist = true;
+    let ComponentExist = true
     fetch(AtolesData)
       .then((res) => {
         res
           .json()
           .then((data) => {
             if (ComponentExist) {
-              setAtole(data.atol);
-              setLoading(false);
+              setAtole(data.atol)
+              setLoading(false)
             }
           })
           .catch(() => {
-            setError(true);
-          });
+            setError(true)
+          })
       })
-      .finally(() => setLoading(false));
+      .finally(() => setLoading(false))
 
     return () => {
-      ComponentExist = false;
-    };
-  }, []);
+      ComponentExist = false
+    }
+  }, [])
 
   if (error) {
-    return <div>Error al obtener los datos. Favor de recargar la página</div>;
+    return <div>Error al obtener los datos. Favor de recargar la página</div>
   }
 
-  if (loading) return <h2>Please wait a moment...</h2>;
+  if (loading) return <h2>Please wait a moment...</h2>
 
   return (
-    <div className="container">
-      <div className="row">
+    <div className='container'>
+      <div className='row'>
         {atole.map((item) => {
-          const { _id, name, img, price } = item;
+          const { _id, name, img, price } = item
 
-          const cantidad = cartItems[_id] ? cartItems[_id].qty : 0;
+          const cantidad = cartItems[_id] ? cartItems[_id].qty : 0
 
-          const onAddToCart = () => addToCart({ id: _id, price ,img});
-          const onRemoveCart = () => removeFromCart({ id: _id, price });
+          const onAddToCart = () => addToCart({ id: _id, price, img })
+          const onRemoveCart = () => removeFromCart({ id: _id, price })
 
           return (
             <Card
@@ -60,12 +60,12 @@ const AtoleIndex = ({addToCart, cartItems, removeFromCart}) => {
               cartItems={cartItems}
               onRemoveCart={onRemoveCart}
             />
-          );
+          )
         })}
       </div>
     </div>
-  );
+  )
 };
 
-export default AtoleIndex;
+export default AtoleIndex
 //
