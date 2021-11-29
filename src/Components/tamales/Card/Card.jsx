7453543367b191d885385201c530dd1038fdde49
Miguel.Contreras <contreras.izquierdo.miguel@gmail.com>
2021-11-28 23:35:47 -0600

    Connected to API, only GET

--- src/Components/tamales/Card/Card.jsx
+++ src/Components/tamales/Card/Card.jsx
@@ -1,45 +1,45 @@
-import React from "react";
-import styles from "./Card.module.scss";
+import React from 'react'
+import styles from './Card.module.scss'
 
-import CardImage from "../../CardImage/CardImage";
-import CardTitle from "../../CardTitle/CardTitle";
+import CardImage from '../../CardImage/CardImage'
+import CardTitle from '../../CardTitle/CardTitle'
 
-import PrimaryButton from "../../buttons/PrimaryButton";
-import Counter from "../../buttons/Counter";
+import PrimaryButton from '../../buttons/PrimaryButton'
+import Counter from '../../buttons/Counter'
 
-const Card = ({ id, titulo, img, price, cantidad, onAddToCart,onRemoveCart }) => {
-  
-  const totalPrice = parseInt(price) * cantidad;
+const Card = ({ id, titulo, img, price, cantidad, onAddToCart, onRemoveCart }) => {
+  const totalPrice = parseInt(price) * cantidad
 
   return (
     <div className={`${styles.card}`}>
-      <CardImage url={img} alt={"Tamal"} />
+      <CardImage url={img} alt='Tamal' />
 
       <div className={`${styles.card_body}`}>
         <CardTitle titulo={titulo} />
 
         <div className={`${styles.precio}`}>Precio por Producto: $ {price}</div>
 
-        {cantidad ? (
-          <Counter
-            id={id}
-            cantidad={cantidad}
-            
-            onAddToCart={onAddToCart}
-            onRemoveCart={onRemoveCart}
-          />
-        ) : (
-          <PrimaryButton
-            text="Comprar"
-            id={id}
-            cantidad={cantidad} 
-            onAddToCart={onAddToCart}
-          />
-        )}
+        {cantidad
+          ? (
+            <Counter
+              id={id}
+              cantidad={cantidad}
+              onAddToCart={onAddToCart}
+              onRemoveCart={onRemoveCart}
+            />
+            )
+          : (
+            <PrimaryButton
+              text='Comprar'
+              id={id}
+              cantidad={cantidad}
+              onAddToCart={onAddToCart}
+            />
+            )}
         <div className={`${styles.total}`}>Total: $ {totalPrice}</div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Card;
+export default Card
